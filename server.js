2887521904d9104aa78486f8f296e23d31a0d9a0
Khawar Ali <khawarali971@gmail.com>
2016-07-21 02:21:19 +0500

    update language

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var middleware = {
 app.use(middleware.logger)
     //app.use(middleware.authenticate)
 app.get('/about', middleware.authenticate, function (req, res) {
-    res.send('"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."');
+    res.send('"Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit... hhello"');
 });
 app.use(express.static(__dirname + '/public'))
 app.listen(port, function () {
